1ef30f54cb8fd4d116f0681c56df3d92e1fb8bf9
GC <GC>
2020-08-07 00:00:10 -0400

    commented routes cleanup

--- Recipes-Api/Services/IRecipeService.cs
+++ Recipes-Api/Services/IRecipeService.cs
@@ -15,25 +15,5 @@ namespace Recipes_Api.Services
         public Task<Recipe> PostRecipe(Recipe recipe);
 
         public Task<bool> DeleteRecipe(long id);
-
-        public Task<IEnumerable<Tool>> GetRecipeTools(long recipeId);
-
-        public Task<Tool> GetRecipeTool(long recipeId, long toolId);
-
-        public Task<IEnumerable<Measurement>> GetRecipeIngredientMeasurements(long recipeId, long ingredientId);
-
-        public Task<Measurement> GetRecipeIngredientMeasurement(long recipeId, long ingredientId, long measurementId);
-
-        public Task PutRecipeIngredientMeasurement(long recipeId, long ingredientId, long measurementId, Measurement measurement);
-
-        public Task<Measurement> PostRecipeIngredientMeasurement(long recipeId, long ingredientId, Measurement measurement);
-
-        public Task<Measurement> DeleteRecipeIngredientMeasurement(long recipeId, long ingredientId, long measurementId);
-
-        public Task PutRecipeTool(long recipeId, long toolId, Tool tool);
-
-        public Task<Tool> PostRecipeTool(Tool tool);
-
-        public Task<Tool> DeleteRecipeTool(long recipeId, long toolId);
     }
 }
