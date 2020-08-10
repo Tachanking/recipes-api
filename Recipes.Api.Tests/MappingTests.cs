92d68fccbbff6a09335eb2c8d8ae2de9ca01f011
GC <GC>
2020-08-10 13:25:49 -0400

    xunit setup

--- Recipes.Api.Tests/MappingTests.cs
+++ Recipes.Api.Tests/MappingTests.cs
@@ -1,14 +1,26 @@
-﻿using AutoMapper;
-using NUnit.Framework;
+﻿using Xunit;
 
 namespace Recipes.Api.Tests
 {
     public class MappingTests
     {
-        //[Test]
-        //public void Map_Should_HaveValidConfig()
-        //{
-        //    Mapper.Initialize(m => m.AddProfile<RecipeProfile>());
-        //}
+        [Fact]
+        public void ROUTENAME_ReturnsTYPE1_WithAListOfTYPE2()
+        {
+            var ok = true;
+            Assert.True(ok);
+        }
+
+        [Fact]
+        public void ROUTENAME_ReturnsTYPE1_WithAListOfTYPE222()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ROUTENAME_ReturnsTYPE1_WithAListOfTYPE333()
+        {
+            Assert.True(true);
+        }
     }
 }
